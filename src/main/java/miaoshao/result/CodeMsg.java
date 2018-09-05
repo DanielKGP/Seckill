a7c791bbf6b9fe170f7056bf1cb3b818d595878b
ganpengkang <ganpengkang@gmail.com>
2018-09-05 11:24:57 -0700

    login page complete

--- src/main/java/miaoshao/result/CodeMsg.java
+++ src/main/java/miaoshao/result/CodeMsg.java
@@ -7,6 +7,15 @@ public class CodeMsg {
     public static CodeMsg SUCCESS = new CodeMsg(0,"success");
     public static CodeMsg SEVER_ERROR = new CodeMsg(500100, "服务端异常");
 
+    //login module
+    public static CodeMsg SESSION_ERROR = new CodeMsg(500210,"Session不存在或者已经失效");
+    public static CodeMsg PASSWORD_EMPTY = new CodeMsg(500211, "登录密码不能为空");
+    public static CodeMsg MOBILE_EMPTY = new CodeMsg(500212, "手机好不能为空");
+    public static CodeMsg MOBILE_ERROR = new CodeMsg(500213, "手机号码格式错误");
+    public static CodeMsg MOBILE_NOT_EXIST = new CodeMsg(500214, "手机号码不存在");
+    public static CodeMsg PASSWORD_ERROR = new CodeMsg(500215, "密码错误");
+
+
     private CodeMsg(int code, String msg){
         this.code = code;
         this.msg = msg;
